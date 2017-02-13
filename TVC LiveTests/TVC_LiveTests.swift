03c6229e8192d5244dc4de688ea54e88626c889c
Gerard Gine <ggine@swiftstack.com>
2017-02-12 20:44:45 -0800

    Initial Commit

---
+++ TVC LiveTests/TVC_LiveTests.swift
@@ -0,0 +1,36 @@
+//
+//  TVC_LiveTests.swift
+//  TVC LiveTests
+//
+//  Created by Gerard Gine on 2/12/17.
+//  Copyright © 2017 Kreative Offensive. All rights reserved.
+//
+
+import XCTest
+@testable import TVC_Live
+
+class TVC_LiveTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
