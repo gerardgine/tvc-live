bf4473c214e0d7e4ce8bb91c92a0fac6d6f55890
Gerard Gine <ggine@swiftstack.com>
2017-02-12 22:52:30 -0800

    3/24, TV3 & 33

--- TVC Live/ViewController.swift
+++ TVC Live/ViewController.swift
@@ -12,10 +12,21 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
-    @IBAction func playVideo(_ sender: Any) {
-        guard let url = URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:324_web/playlist.m3u8") else {
+    @IBAction func playVideo(_ sender: UIButton) {
+        guard let title = sender.currentTitle else {
             return
         }
+        let urls : [String:URL] = [
+            "3/24": URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:324_web/playlist.m3u8")!,
+            "TV3": URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:tv3_web/playlist.m3u8")!,
+            "33": URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:c33_web/playlist.m3u8")!
+        ]
+        guard let url = urls[title] else {
+            return
+        }
+//        guard let url = URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:324_web/playlist.m3u8") else {
+//            return
+//        }
         // Create an AVPlayer, passing it the HTTP Live Streaming URL.
         let player = AVPlayer(url: url)
         
