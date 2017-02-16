5a89a85e4688b11fff7874a29c36a8a9e19f78cb
Gerard Gine <ggine@swiftstack.com>
2017-02-16 01:08:24 -0800

    Buttons with images
    By now, buttons are uniquely identified by tag, but I guess there is another way to do it.

--- TVC Live/ViewController.swift
+++ TVC Live/ViewController.swift
@@ -13,11 +13,17 @@ import AVFoundation
 class ViewController: UIViewController {
 
     @IBAction func playVideo(_ sender: UIButton) {
-        let international_urls : [String:URL] = [
-            "3/24": URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:324_web/playlist.m3u8")!,
-            "TV3": URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:tv3_web/playlist.m3u8")!,
-            "33": URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:c33_web/playlist.m3u8")!
+        let international_urls : [URL] = [
+            URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:tv3_web/playlist.m3u8")!,
+            URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:324_web/playlist.m3u8")!,
+            URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:c33_web/playlist.m3u8")!
         ]
+        
+//        let international_urls : [String:URL] = [
+//            "3/24": URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:324_web/playlist.m3u8")!,
+//            "TV3": URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:tv3_web/playlist.m3u8")!,
+//            "33": URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:c33_web/playlist.m3u8")!
+//        ]
         
 //        let spanish_urls : [String:URL] = [
 //            "3/24": URL(string: "http://ccma-tva-int-abertis-live.hls.adaptive.level3.net/int/ngrp:324_web/playlist.m3u8")!,
@@ -25,14 +31,14 @@ class ViewController: UIViewController {
 //            "33": URL(string: "http://ccma-tva-es-abertis-live.hls.adaptive.level3.net/es/ngrp:c33_web/playlist.m3u8")!,
 //            "Esport3": URL(string: "http://ccma-tva-es-abertis-live.hls.adaptive.level3.net/es/ngrp:es3_web/playlist.m3u8")!
 //        ]
+
+        let title = sender.tag
         
-        guard let title = sender.currentTitle else {
-            return
-        }
+//        guard let url = international_urls[title] else {
+//            return
+//        }
         
-        guard let url = international_urls[title] else {
-            return
-        }
+        let url = international_urls[title]
 
         // Create an AVPlayer, passing it the HTTP Live Streaming URL.
         let player = AVPlayer(url: url)
